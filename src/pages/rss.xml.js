87ce17f7e6344e838a4fb3a231c79c5d53fa1dfb
Shinya Hara <rabby.coffee.c5@gmail.com>
2024-05-19 21:54:56 +0900

    ✨ コンテンツ コレクションによる拡張

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
+  const posts = await getCollection("posts");
   return rss({
     title: "Astro学習者 | ブログ",
     description: "Astroを学ぶ旅",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `${import.meta.env.BASE_URL}/posts/${post.slug}/`,
+    })),
     customData: `<language>ja-jp</language>`,
   });
 }
